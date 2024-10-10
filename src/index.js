5a3d78ecc57709919b593b73fe30a452da0d5f43
MD Rashid Alam <alamr9253@gmail.com>
2024-10-11 01:31:16 +0530

    Tested user schema

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ const express=require("express")
 // const badyParser=require('body-parser')
 
 const ServerConfig= require('./config/serverConfig')
-const connectDB=require('./config/dbConfig')
+const connectDB=require('./config/dbConfig');
+// const User = require("./schema/userSchema");
 
 const app=express();
 
@@ -14,9 +15,20 @@ app.use(express.urlencoded({extended: true}))
 app.post('/ping',(req,res)=>{
     console.log(req.body); 
     return res.json({message: 'pong'});
-})
+});
 
 app.listen(ServerConfig.PORT,async ()=>{
     await connectDB();
-    console.log(`Server started at port ${ServerConfig.PORT}`)
-})
+    console.log(`Server started at port ${ServerConfig.PORT}`);
+
+    // const newUser=await User.create({
+    //     email:"a@fs.com",
+    //     password:'12345656',
+    //     lastName:"mdalamrff",
+    //     firstName:"rashidsdfg",
+    //     mobilNumber:'9110174498',
+
+    // });
+    // console.log('Create new user .');
+    // console.log(newUser);
+});
